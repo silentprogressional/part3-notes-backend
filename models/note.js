9c372a456e2ba196605884103ef248239230a9cf
unknown <astrupnate@gmail.com>
2020-11-03 15:08:20 -0500

    refactoring modules

--- models/note.js
+++ models/note.js
@@ -1,36 +1,24 @@
-const mongoose = require('mongoose')
-mongoose.set('useFindAndModify', false)
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+const mongoose = require("mongoose");
 
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
+    required: true,
     minlength: 5,
-    required: true
   },
-  date: { 
+  date: {
     type: Date,
-    required: true
+    required: true,
   },
-  important: Boolean
-})
+  important: Boolean,
+});
 
-noteSchema.set('toJSON', {
+noteSchema.set("toJSON", {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema)
+module.exports = mongoose.model("Note", noteSchema);
